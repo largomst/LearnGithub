d2c43ae689124b8ca721dcb7bae282b51f9c6efa
largomst <dingrui.zhen@gmail.com>
2017-03-03 23:21:45 +0800

    fixed topic and code

--- solution.py
+++ solution.py
@@ -1,9 +1,21 @@
 def Solution(arr, n):
-    new_arr = []
+    j = 0
     for i in range(len(arr)):
-        if arr[i] <= n:
-            new_arr.append(arr[i])
-
-    return new_arr
+        if arr[i] == n:
+            continue
+        arr[j] = arr[i]
+        j += 1
+    return len(arr)
 
 print(Solution([3, 5, 6, 2, 96, 34, 32], 20))
+
+# Given an array and a value, remove all instances of that value in place
+# and return the new length.
+
+# Do not allocate extra space for another array, you must do this in place
+# with constant memory.
+
+# The order of elements can be changed. It doesn't matter what you leave
+# beyond the new length.
+
+# 楼主的题目居然都给错了，妈蛋
